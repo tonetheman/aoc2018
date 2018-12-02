9e41b8453428e0d357749c383eac28cc9ac78b34
tonetheman <tonetheman@gmail.com>
2018-12-02 13:48:41 -0600

    nevermind

--- day2/py/weirdtree.py
+++ day2/py/weirdtree.py
@@ -1,9 +1,38 @@
 
 
+"""
 import string
 data = open("day2data.txt","r").readlines()
 fdata= []
 for d in data:
    fdata.append(d.strip("\r\n"))
-print(fdata)
+# print(fdata)
+"""
 
+fdata = [
+"abcde",
+"fghij",
+"klmno",
+"pqrst",
+"fguij",
+"axcye",
+"wvxyz"
+]
+
+class Pointer:
+    def __init__(self,index,value):
+        self.index = index
+        self.value = value
+
+first = {}
+for word in fdata:
+    firstchar = word[0]
+    if firstchar in first:
+        first[firstchar] = first[firstchar] + 1
+    else:
+        first[firstchar] = 1
+
+print(first)
+
+for word in fdata:
+    secondchar = word[1]
