d06cd6bf9d08fa38bde3d8b69d88359278db053a
tonetheman <tonetheman@gmail.com>
2018-12-02 05:51:19 +0000

    one more time

--- day2/py/prog.py
+++ day2/py/prog.py
@@ -20,5 +20,25 @@ def chk(data):
     def has3(s):
         return hasit(s,3)
     return len(filter(has2,data)) * len(filter(has3,data))
+def chk2(data):
+    def hasit(s,VV):
+        cc = {}
+        for c in s:
+            if cc.has_key(c):
+                cc[c]=cc[c]+1
+            else:
+                cc[c]=1
+        for k in cc.keys():
+            if cc[k]==VV:
+                return 1
+        return 0
+    def has2(s):
+        return hasit(s,2)
+    def has3(s):
+        return hasit(s,3)
+    def addr(a,b):
+        return a+b
+    return reduce(addr,map(has3,data)) * reduce(addr,map(has2,data))
 
-print("res", chk(data))
+
+print("res", chk2(data))
