2c94c411c32f4d7727c7fd34a211643d69c2b778
tonetheman <tonetheman@gmail.com>
2018-12-03 13:30:12 +0000

    working on parsing data still sigh

--- day3/gocode/prog.go
+++ day3/gocode/prog.go
@@ -36,25 +36,20 @@ func scan(str string) {
 	}
 }
 
-// splits the string...
-// FUCK regex in go this is easy in python
-func regexTest(str string) []string {
-	res := make([]string, 0)
-	P := regexp.MustCompile("^#[0-9]+ @ [0-9]+,[0-9]+: [0-9]+x[0-9]+")
-	firstone := P.FindString(str)
-	res = append(res, firstone)
-	//fmt.Println(firstone)
-	data := P.Split(str, -1)
-	//fmt.Println(data)
-	for i := 0; i < len(data); i++ {
-		res = append(res, data[i])
-	}
-	return res
+//
+func rt(str string) []string {
+	P := regexp.MustCompile("#([0-9]+) @ [0-9]+,[0-9]+: [0-9]+x[0-9]+\n")
+	all := P.FindAllString(str, -1) // this is all of the items
+	return all
 }
 
 func main() {
 	bytedata := readfile("../input")
 	stringdata := bytesToString(bytedata)
-	splitData := regexTest(stringdata)
-	fmt.Println(splitData)
+	data := rt(stringdata)
+	t := strings.Split(data[0], " ")
+	fmt.Println(len(t), t)
+
+	//splitData := regexTest(stringdata)
+	//fmt.Println(splitData)
 }
