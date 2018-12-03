7e878028b812c6a83666cd248e6e48ba15cdc55c
tonetheman <tonetheman@gmail.com>
2018-12-03 14:24:17 +0000

    parsing finally working

--- day3/gocode/prog.go
+++ day3/gocode/prog.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -27,29 +27,40 @@ func bytesToString(buffer []byte) string {
 	return string(buffer)
 }
 
-func scan(str string) {
-	scanner := bufio.NewScanner(strings.NewReader(str))
-	scanner.Split(bufio.ScanWords)
-	for i := 0; i < 4; i++ {
-		fmt.Println(scanner.Scan())
-		fmt.Println(scanner.Text())
-	}
-}
-
-//
 func rt(str string) []string {
 	P := regexp.MustCompile("#([0-9]+) @ [0-9]+,[0-9]+: [0-9]+x[0-9]+\n")
 	all := P.FindAllString(str, -1) // this is all of the items
 	return all
 }
 
+func parseone(s string) cloth {
+	//fmt.Println("starting s", s)
+	whdata := strings.Split(s, "#")
+	withouthash := whdata[1]
+	spdata := strings.Split(withouthash, " ")
+	id, _ := strconv.Atoi(spdata[0])
+	posxystring := spdata[2]
+	widthheightstring := spdata[3]
+	pdata := strings.Split(posxystring, ",")
+	posx, _ := strconv.Atoi(pdata[0])
+	posy, _ := strconv.Atoi(strings.TrimRight(pdata[1], ":"))
+	_whd := strings.Split(widthheightstring, "x")
+	width, _ := strconv.Atoi(_whd[0])
+	height, _ := strconv.Atoi(_whd[1])
+	return cloth{id, posx, posy, width, height}
+}
+
+func parseall(s []string) []cloth {
+	res := make([]cloth, 0)
+	for i := 0; i < len(s); i++ {
+		res = append(res, parseone(s[i]))
+	}
+	return res
+}
+
 func main() {
 	bytedata := readfile("../input")
 	stringdata := bytesToString(bytedata)
 	data := rt(stringdata)
-	t := strings.Split(data[0], " ")
-	fmt.Println(len(t), t)
-
-	//splitData := regexTest(stringdata)
-	//fmt.Println(splitData)
+	fmt.Println(parseall(data))
 }
