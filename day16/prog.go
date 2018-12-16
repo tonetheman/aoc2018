3a33b98aaaff52c0647fb4447e5d9450360ced70
tonetheman <tonetheman@gmail.com>
2018-12-16 14:31:07 +0000

    all instructions implemented

--- day16/prog.go
+++ day16/prog.go
@@ -29,8 +29,11 @@ func readfile(filename string) []byte {
 	return filedata
 }
 
-func addr(inreg _registers, oper _instr) {
-
+func addr(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = inreg[oper.a] + inreg[oper.b]
+	return res
 }
 func addi(inreg _registers, oper _instr) _registers {
 	res := _registers{}
@@ -38,35 +41,145 @@ func addi(inreg _registers, oper _instr) _registers {
 	res[oper.output] = inreg[oper.a] + oper.b
 	return res
 }
-
-func mulr(inreg _registers, oper _instr) {
-	inreg[oper.output] = inreg[oper.a] * inreg[oper.b]
+func mulr(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = inreg[oper.a] * inreg[oper.b]
+	return res
+}
+func muli(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = inreg[oper.a] * oper.b
+	return res
 }
 
-func seti(inreg _registers, oper _instr) {
-	inreg[oper.output] = inreg[oper.a]
+// bitwise and
+func banr(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = inreg[oper.a] & inreg[oper.b]
+	return res
+}
+func bani(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = inreg[oper.a] & oper.b
+	return res
+}
+
+// bitwise or
+func borr(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = inreg[oper.a] | inreg[oper.b]
+	return res
+}
+func bori(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = inreg[oper.a] | oper.b
+	return res
+}
+
+// assignment
+func seti(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = oper.a // value
+	return res
+}
+func setr(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	res[oper.output] = inreg[oper.a]
+	return res
+}
+
+// greater than testing
+func gtir(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	if oper.a > inreg[oper.b] {
+		res[oper.output] = 1
+	} else {
+		res[oper.output] = 0
+	}
+	return res
+}
+func gtri(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	if inreg[oper.a] > oper.b {
+		res[oper.output] = 1
+	} else {
+		res[oper.output] = 0
+	}
+	return res
+}
+func gtrr(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	if inreg[oper.a] > inreg[oper.b] {
+		res[oper.output] = 1
+	} else {
+		res[oper.output] = 0
+	}
+	return res
+}
+
+// equality testing
+func eqir(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	if oper.a == inreg[oper.b] {
+		res[oper.output] = 1
+	} else {
+		res[oper.output] = 0
+	}
+	return res
+}
+func eqri(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	if inreg[oper.a] == oper.b {
+		res[oper.output] = 1
+	} else {
+		res[oper.output] = 0
+	}
+	return res
+}
+func eqrr(inreg _registers, oper _instr) _registers {
+	res := _registers{}
+	res.copy(inreg)
+	if inreg[oper.a] == inreg[oper.b] {
+		res[oper.output] = 1
+	} else {
+		res[oper.output] = 0
+	}
+	return res
 }
 
 func testAddi() {
 	fmt.Println("testAddi")
 	inreg := _registers{3, 2, 1, 1}
 	fmt.Println("addi enter", inreg)
-	addi(inreg, _instr{9, 2, 1, 2})
-	fmt.Println("addi exit", inreg)
+	outreg := addi(inreg, _instr{9, 2, 1, 2})
+	fmt.Println("addi exit", outreg)
 }
 func testMulr() {
 	fmt.Println("testMulr")
 	inreg := _registers{3, 2, 1, 1}
 	fmt.Println("mulr enter", inreg)
-	mulr(inreg, _instr{9, 2, 1, 2})
-	fmt.Println("mulr exit", inreg)
+	outreg := mulr(inreg, _instr{9, 2, 1, 2})
+	fmt.Println("mulr exit", outreg)
 }
 func testSeti() {
 	fmt.Println("testSeti")
 	inreg := _registers{3, 2, 1, 1}
 	fmt.Println("seti enter", inreg)
-	seti(inreg, _instr{9, 2, 1, 2})
-	fmt.Println("seti exit", inreg)
+	outreg := seti(inreg, _instr{9, 2, 1, 2})
+	fmt.Println("seti exit", outreg)
 }
 
 func examples() {
@@ -76,4 +189,5 @@ func examples() {
 }
 
 func main() {
+	examples()
 }
